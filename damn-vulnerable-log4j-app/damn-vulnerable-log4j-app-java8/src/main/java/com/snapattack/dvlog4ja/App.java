9113cc4d24440c978c60f0db73215944a3af1789
Dinesh Mistry <dinesh@dman.cloud>
2024-03-07 11:00:57 -0500

    removed Java8 vuln

--- damn-vulnerable-log4j-app/damn-vulnerable-log4j-app-java8/src/main/java/com/snapattack/dvlog4ja/App.java
+++
@@ -1,9 +0,0 @@
-package com.snapattack.dvlog4ja;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class App extends Application {
-    // the entry class - deliberately empty in this basic demo
-}
